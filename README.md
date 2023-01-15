e10d110388dc8ff03efb9af1e7aa8f2ace4d179d
Abdullah Othman <77742926+OthmanAbdullah@users.noreply.github.com>
2023-01-15 04:16:10 +0100

    Created solution for the letter combinations problem

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# letter-combinations-problem-solution
+## Phone Number Combinations
+
+
+
+<h3><em>How to run the test cases:</em> </h3>
+<p>The libraries required for testing are included in this repo.
+<br> 
+To run the tests you can use the following commands. (If you're using Linux, use `:` instead of `;`.)
+
+    ```
+    javac -cp .;junit-4.13.jar src/PhoneNumber.java
+    java -cp .;junit-4.13.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore testing.PhoneNumberTest
+    ```
+</p>
+
+
+
+
--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,45 +1,76 @@
-package src;
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * PhoneNumber class contains a single static method "letterCombinations" that takes a String as an input and returns a List of Strings.
+ * The input string represents a phone number, the output list represents all possible letter combinations that the given phone number can represent,
+ * where each digit maps to a set of letters. The returned list is sorted in lexicographic order.
+ *
+ * @author (Abdullah Othman)
+ * @version (1/15/2023)
+ */
 
-public class PhoneNumber {
-    public static List<String> letterCombinations(String digits) {
-        if (digits.length() == 0)
-            return new ArrayList<>();
-        String[] phoneNumberMap = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" }; // the digit is  the  corrosponding  index
-        List<String> allPossibleLetterCombinations = new ArrayList<>();
-        backtrack("", 0, digits, allPossibleLetterCombinations, phoneNumberMap);
-        return allPossibleLetterCombinations;
-    }
-
-    private static void backtrack(String combination, int nextDigitIdx, String next_digits, List<String> result,
-            String[] phone_number_map) {
-        if (next_digits.length() == nextDigitIdx) { // stopping condition of the recursion.
-            result.add(combination);
-            return;
-        }
-        String letters = phone_number_map[next_digits.charAt(nextDigitIdx) - '0'];
-        for (int i = 0; i < letters.length(); i++) {
-            char letter = letters.charAt(i);
-            backtrack(combination + letter, nextDigitIdx + 1, next_digits, result, phone_number_map);
-        }
-    }
-    
-    // Note: the following funciton is not used because the letterCombination function produces the result in lexicographic order.
-    // However, it could be used in case sorting is needed, such as going with a brute-force way.  
-    public static void insertionSort(List<String> list) {
-        for (int i = 1; i < list.size(); i++) {
-            String current = list.get(i);
-            int j = i - 1;
-            while (j >= 0 && list.get(j).compareTo(current) > 0) {
-                list.set(j + 1, list.get(j));
-                j--;
-            }
-            list.set(j + 1, current);
-        }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(letterCombinations("23"));
-    }
-}
+ package src;
+ import java.util.ArrayList;
+ import java.util.List;
+ 
+ 
+ public class PhoneNumber {
+     /**
+      * This method takes a phone number as a string, and returns a list of all possible letter combinations that the phone number can represent.
+      * Each digit maps to a set of letters. The returned list is sorted in lexicographic order.
+      * @param digits a string representation of the phone number.
+      * @return a list of all possible letter combinations that the phone number can represent.
+      */
+     public static List<String> letterCombinations(String digits) {
+         if (digits.length() == 0)
+             return new ArrayList<>();
+         String[] phoneNumberMap = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" }; // the digit is  the  corrosponding  index
+         List<String> allPossibleLetterCombinations = new ArrayList<>();
+         backtrack("", 0, digits, allPossibleLetterCombinations, phoneNumberMap);
+         return allPossibleLetterCombinations;
+     }
+ 
+     /**
+      * A helper function that uses recursion to generate all possible letter combinations.
+      *
+      * @param combination  The current combination of letters
+      * @param nextDigitIdx  The next index of the digit to process
+      * @param digits  The  digits to process
+      * @param result  A list to store the final letter combinations
+      * @param phoneNumberMap  A mapping of digits to letters
+      */
+     private static void backtrack(String combination, int nextDigitIdx, String digits, List<String> result,
+             String[] phoneNumberMap) {
+         if (digits.length() == nextDigitIdx) { // stopping condition of the recursion.
+             result.add(combination);
+             return;
+         }
+         String letters = phoneNumberMap[digits.charAt(nextDigitIdx) - '0'];
+         for (int i = 0; i < letters.length(); i++) {
+             char letter = letters.charAt(i);
+             backtrack(combination + letter, nextDigitIdx + 1, digits, result, phoneNumberMap);
+         }
+     }
+ 
+     /**
+      * This method sorts a List of Strings in lexicographic order.
+      *
+      * @param list a List of Strings to sort
+      */
+     // Note: the following funciton is not used because the letterCombination function produces the result in lexicographic order.
+     // However, it could be used in case sorting is needed, such as going with a brute-force way.  
+     public static void insertionSort(List<String> list) {
+         for (int i = 1; i < list.size(); i++) {
+             String current = list.get(i);
+             int j = i - 1;
+             while (j >= 0 && list.get(j).compareTo(current) > 0) {
+                 list.set(j + 1, list.get(j));
+                 j--;
+             }
+             list.set(j + 1, current);
+         }
+     }
+ 
+     public static void main(String[] args) {
+         System.out.println(letterCombinations("23"));
+     }
+ }
+ 
